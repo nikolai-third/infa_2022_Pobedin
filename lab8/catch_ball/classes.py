84a781bfd4589669fe405bffcc7a47500f1a19de
Pobedin Nikolai <k.pobedin2004@gmail.com>
2022-11-08 00:11:17 +0300

    ура! появились квадраты!

--- lab8/catch_ball/classes.py
+++ lab8/catch_ball/classes.py
@@ -5,21 +5,22 @@ import random
 class Ball(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.radius = random.randint(30, 80)
-        self.image = pygame.Surface((2 * self.radius, 2 * self.radius))
-        self.image.set_alpha(255)
+        self.radius = random.randint(30, 80)  # радиус шара
+        self.image = pygame.Surface((2 * self.radius, 2 * self.radius))  # объект - поверхность - квадрат со стороной 2радиуса
+        self.image.set_alpha(255)  # делает поверхность прозрачной
         self.rect = self.image.get_rect()
-        self.rect.x = random.randrange(100, 800)
+        self.rect.x = random.randrange(100, 800)  # генерация координат шара
         self.rect.y = random.randrange(100, 400)
-        pygame.draw.circle(self.image, (random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255)),
-                           (self.radius, self.radius), self.radius)
-        self.speedy = random.randrange(-8, 8)
+        pygame.draw.circle(self.image, (random.randrange(0, 240), random.randrange(0, 240), random.randrange(0, 240)),
+                           (self.radius, self.radius), self.radius)  # на поверхности генерируется круг случаного цвета
+        self.speedy = random.randrange(-8, 8)  # генерируется случайная скорость
         self.speedx = random.randrange(-8, 8)
 
     def update(self):
-        self.rect.x += self.speedx
+        self.rect.x += self.speedx  # на каждом кадре координаты равны предыдущим + значение скорости
         self.rect.y += self.speedy
-        if self.rect.top < 0:
+
+        if self.rect.top < 0:  # при столкновении шара со стенкой, то его скорость генерируется случайно
             self.speedx = random.randrange(-8, 8)
             self.speedy = random.randrange(0, 8)
 
@@ -40,7 +41,56 @@ class Ball(pygame.sprite.Sprite):
         return result
 
 
-def strike(xm, ym, C):
-    if (xm - C[0])**2 + (ym - C[1])**2 <= C[2]**2:
-        return True
-    return False
+class Square(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.length = random.randint(15, 50)  # половина длины стороны квадрата
+        self.image = pygame.Surface((2 * self.length, 2 * self.length))
+        self.image.fill((random.randrange(0, 240), random.randrange(0, 240), random.randrange(0, 240)))
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(100, 800)  # генерация координат квадрата
+        self.rect.y = random.randrange(100, 400)
+        self.speedy = random.randrange(-8, 8)  # генерируется случайная скорость
+        self.speedx = random.randrange(-8, 8)
+
+    def update(self):
+        self.rect.x += self.speedx  # на каждом кадре координаты равны предыдущим + значение скорости
+        self.rect.y += self.speedy
+
+        if self.rect.top < 0:  # при столкновении шара со стенкой, то происходит упругое столкновение
+            self.speedy = -self.speedy
+
+        if self.rect.bottom > 500:
+            self.speedy = -self.speedy
+
+        if self.rect.right > 900:
+            self.speedx = -self.speedx
+
+        if self.rect.left < 0:
+            self.speedx = -self.speedx
+
+    def coordinate(self):
+        result = (self.rect.centerx, self.rect.centery, self.length)
+        return result
+
+
+def strike(form, xm, ym, C):
+    '''
+    вызывается при нажатии левой кнопкой мыши
+    проверяет попал ли пользователь по объекту
+    :param form: b - шары, s - квадраты
+    :param xm: координаты по x клика мыши
+    :param ym: координаты по y клика мыши
+    :param C: кортеж данных про объект по которому можно проверить попал ли пользователь по объекту
+    :return: True если пользователь попал и False если не попал
+    '''
+    if form == 's':
+        if abs((xm - C[0])) < C[2] and abs((ym - C[1])) < C[2]:
+            return True
+        else:
+            return False
+    if form == 'b':
+        if (xm - C[0])**2 + (ym - C[1])**2 <= C[2]**2:
+            return True
+        else:
+            return False
--- lab8/catch_ball/main.py
+++ lab8/catch_ball/main.py
@@ -1,5 +1,5 @@
 import pygame
-from classes import Ball, strike
+from classes import Ball, Square, strike
 import os
 
 os.chdir("/Users/nort/Desktop/infa_2022_Pobedin/lab8/catch_ball")
@@ -7,50 +7,56 @@ os.chdir("/Users/nort/Desktop/infa_2022_Pobedin/lab8/catch_ball")
 pygame.init()
 
 FPS = 45
-screen = pygame.display.set_mode((900, 500))
-
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
-GREEN = (0, 255, 0)
-MAGENTA = (255, 0, 255)
-CYAN = (0, 255, 255)
-BLACK = (0, 0, 0)
-COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
+screen = pygame.display.set_mode((900, 500))  # размер экрана
 
 
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
 
-balls = pygame.sprite.Group()
-for i in range(10):
+all_sprite = pygame.sprite.Group()
+balls = pygame.sprite.Group()  # создаем 5 экземпляров класса Ball и 5 Square
+squares = pygame.sprite.Group()
+
+for i in range(5):
+    s = Square()
+    squares.add(s)
+    all_sprite.add(s)
+
+for i in range(5):
     b = Ball()
     balls.add(b)
+    all_sprite.add(b)
 
-points = 0
-
-u = 0
+points = 0  # количество очков
 
 while not finished:
     clock.tick(FPS)
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT:  # если нажать на крестик, то игра завершится
             finished = True
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN:  # если пользователь нажимает левую кнопку мыши, то проверяется попал ли игрок по цели
             if event.button == 1:
-                data = []
-                for i in balls:
-                    if strike(event.pos[0], event.pos[1], i.coordinate()):
-                        i.kill()
-                        points += 1
-                        b = Ball()
-                        balls.add(b)
+                for i in all_sprite:
+                    if i in balls:
+                        if strike('b', event.pos[0], event.pos[1], i.coordinate()):
+                            i.kill()
+                            points += 1
+                            b = Ball()
+                            balls.add(b)
+                            all_sprite.add(b)
+                    elif i in squares:
+                        if strike('s', event.pos[0], event.pos[1], i.coordinate()):
+                            i.kill()
+                            points += 1.5
+                            s = Square()
+                            squares.add(s)
+                            all_sprite.add(s)
 
-    balls.update()
+    all_sprite.update()
     pygame.display.update()
-    screen.fill(BLACK)
-    balls.draw(screen)
+    screen.fill((0, 0, 0))
+    all_sprite.draw(screen)
     pygame.display.flip()
 
 print(points)
